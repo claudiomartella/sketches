319e2fa125dc20ddaa43630bc15330db2ddcb79d
Claudio Martella <claudio.martella@gmail.com>
2011-02-25 15:30:32 +0100

    license added

--- src/main/java/org/acaro/sketches/Sketch.java
+++ src/main/java/org/acaro/sketches/Sketch.java
@@ -1,12 +1,27 @@
+/*Copyright 2011 Claudio Martella
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
 package org.acaro.sketches;
 
 import java.nio.ByteBuffer;
 
 public interface Sketch {
-	final static byte THROWUP = 1;
-	final static byte BUFF = 2;
-	// sizeof(byte)+sizeof(long)+sizeof(short)+sizeof(int)
-	final static int HEADER_SIZE = 15;
+	static final byte THROWUP = 1;
+	static final byte BUFF = 2;
+	static final int HEADER_SIZE = Util.SIZEOF_BYTE+Util.SIZEOF_LONG+Util.SIZEOF_SHORT+Util.SIZEOF_INT;
 	
 	public byte[] getKey();
 	
--- src/main/java/org/acaro/sketches/Wall.java
+++ src/main/java/org/acaro/sketches/Wall.java
@@ -1,11 +1,26 @@
+/*Copyright 2011 Claudio Martella
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
 package org.acaro.sketches;
 
 public class Wall {
 	//private BookIndex index;
-	// sizeof(byte)+sizeof(long)
-	final public static int HEADER_SIZE = 1+8;
-	final public static byte CLEAN = 1;
-	final public static byte DIRTY = 2;
+	static final public int HEADER_SIZE = Util.SIZEOF_BYTE+Util.SIZEOF_LONG;
+	static final public byte CLEAN = 1;
+	static final public byte DIRTY = 2;
 	
 	public Sketch get(byte[] key) {
 		Sketch s = null;
