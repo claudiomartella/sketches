42aaf73206c28bbc509bcbb28279c3dcc1d27de0
Claudio Martella <claudio.martella@gmail.com>
2011-05-23 11:40:54 +0200

    naming changes

--- src/main/java/org/acaro/sketches/Sketches.java
+++ src/main/java/org/acaro/sketches/Sketches.java
@@ -11,26 +11,34 @@
    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
    See the License for the specific language governing permissions and
    limitations under the License.
-*/
+ */
 
 package org.acaro.sketches;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-import org.acaro.sketches.mindsketches.MindSketches;
-import org.acaro.sketches.mural.FSMural;
-import org.acaro.sketches.sketch.Buff;
-import org.acaro.sketches.sketch.Sketch;
-import org.acaro.sketches.sketch.Throwup;
-import org.acaro.sketches.sketchbook.BufferedSketchBook;
-import org.acaro.sketches.sketchbook.SketchBook;
+import org.acaro.sketches.io.OperationReader;
+import org.acaro.sketches.io.OperationWriter;
+import org.acaro.sketches.memstore.Memstore;
+import org.acaro.sketches.operation.Operation;
+import org.acaro.sketches.operation.Update;
+import org.acaro.sketches.sfile.RAMSFile;
+import org.acaro.sketches.sfile.SFile;
+import org.acaro.sketches.util.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 
 /**
- * 
  * 
  * The class is the entry point to the KV store.
  * 
@@ -42,115 +50,217 @@ import com.google.common.base.Preconditions;
  */
 
 public class Sketches {
-	private MindSketches memory;
-	private SketchBook book;
-	private List<FSMural> murals = new ArrayList<FSMural>();
+	private static final Logger logger = LoggerFactory.getLogger(OldSketches.class);
+
+	private OperationWriter writer;
+	private final List<OperationReader> readers = new ArrayList<OperationReader>();
+	/* 
+	 * This lock protects modification of readers and writer, not the modification of a contained Sketch.
+	 * - lock for read when you access readers or writer
+	 * - lock for write when you modify readers (at bombing & scribing) and writer reference 
+	 */
+	private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+	private final Lock readLock  = lock.readLock();
+	private final Lock writeLock = lock.writeLock();
+
+	private final Configuration conf = Configuration.getConf();
+
+	// this will flush the log every 10 seconds by default. It stops at first Exception
+	private final ScheduledExecutorService flushScheduler = Executors.newScheduledThreadPool(1);
+	private final ScheduledFuture<?> flusherHandle = flushScheduler.scheduleAtFixedRate(
+																	new Flusher(), 
+																	conf.getInt("sketches.flusherdelay", 10000), 
+																	conf.getInt("sketches.flusherdelay", 10000), 
+																	TimeUnit.MILLISECONDS);
+
+	// this will try to scribe the Memestore if it's bigger than default 64MB. It stops at first Exception
+	private final ScheduledExecutorService scriberScheduler = Executors.newScheduledThreadPool(1);
+	private final ScheduledFuture<?> scriberHandle = scriberScheduler.scheduleAtFixedRate(
+																	  new Scriber(conf.getInt("sketches.memstore.maxsize", 64)), 
+																	  conf.getInt("sketches.scriberdelay", 30), 
+																	  conf.getInt("sketches.scriberdelay", 30), 
+																	  TimeUnit.SECONDS);
+
+	private final ScheduledExecutorService compactorScheduler = Executors.newScheduledThreadPool(1);
+	private ScheduledFuture<?> compactorHandle;
+	
 	private String path;
 	private String name;
-	
+
 	public Sketches(String path, String name) throws IOException {
+		Preconditions.checkNotNull(path);
+		Preconditions.checkNotNull(name);
 		this.path = path;
 		this.name = name;
-		initSketchBook(path, name);
-		initMurals(path, name);
+		init(path, name);
 	}
-	
+
 	public void put(byte[] key, byte[] value) throws IOException {
 		Preconditions.checkNotNull(key);
 		Preconditions.checkNotNull(value);
 		Preconditions.checkArgument(key.length <= Short.MAX_VALUE, "key length can not be bigger than %s", Short.MAX_VALUE);
-		
-		Throwup t = new Throwup(key, value);
-		book.write(t);
-		memory.put(key, t);
+
+		doPut(key, new Update(key, value));
 	}
-	
+
 	public byte[] get(byte[] key) throws IOException {
 		Preconditions.checkNotNull(key);
 		Preconditions.checkArgument(key.length <= Short.MAX_VALUE, "key length can not be bigger than %s", Short.MAX_VALUE);
-		
-		Sketch s;
+
+		Operation o;
 		byte[] value = null;
-		
-		if ((s = doGet(key)) != null) { // it was here... 
-			if (s instanceof Buff) { // but it was deleted
-				value = null;
-			} else if (s instanceof Throwup) { // and we found it
-				value = ((Throwup) s).getValue();
-			}
-		}
+
+		if ((o = doGet(key)) != null) // it was here... 
+			value = o.getValue();
 		
 		return value;
 	}
-	
-	public byte[] getAndDelete(byte[] key) throws IOException {
-		Preconditions.checkNotNull(key);
-		Preconditions.checkArgument(key.length <= Short.MAX_VALUE, "key length can not be bigger than %s", Short.MAX_VALUE);
-		
-		Sketch s;
-		byte[] value = null;
-		
-		// it's still around
-		if ((s = doGet(key)) != null && !(s instanceof Buff)) {
-			doDelete(key);
-			value = ((Throwup) s).getValue();
-		}
-		
-		return value;
-	}
-	
+
 	public void delete(byte[] key) throws IOException {
 		Preconditions.checkNotNull(key);
 		Preconditions.checkArgument(key.length <= Short.MAX_VALUE, "key length can not be bigger than %s", Short.MAX_VALUE);
-		
+
 		doDelete(key);
 	}
-	
+
 	public void shutdown() throws IOException {
-		book.close();
+		flusherHandle.cancel(false); // will these block or fail?
+		scriberHandle.cancel(false); 
+		
+		// stop everthing and close things
 	}
 
-	public void bomb() throws IOException {
-		MindSketches oldMemory = memory;
-		memory = new MindSketches();
-		SketchesHelper.bombMindSketches(oldMemory, path+"/"+name+".br");
+	private Operation doGet(byte[] key) throws IOException {
+		Operation o = null;
+
+		readLock.lock();
+
+		try {
+
+			for (OperationReader reader: readers)
+				if ((o = reader.get(key)) != null)
+					break;
+
+		} finally {
+			readLock.unlock();
+		}
+
+		return o;
+	}
+
+	private void doPut(byte[] key, Operation o) throws IOException {
+
+		readLock.lock();
+
+		try {
+
+			writer.put(key, o);
+
+		} finally {
+			readLock.unlock();
+		}
 	}
 	
 	private void doDelete(byte[] key) throws IOException {
-		Buff b = new Buff(key);
-		book.write(b);
-		memory.put(key, b);
-	}
-	
-	private Sketch doGet(byte[] key) throws IOException {
-		Sketch s;
-		
-		if ((s = memory.get(key)) == null) {
-			s = muralsGet(key);
+
+		readLock.lock();
+
+		try {
+
+			writer.delete(key);
+
+		} finally {
+			readLock.unlock();
 		}
-		
-		return s;
 	}
 	
-	private Sketch muralsGet(byte[] key) throws IOException {
-		Sketch s = null;
-		
-		for (FSMural w: murals) {
-			if ((s = w.get(key)) != null) {
-				break;
-			}
-		}
-		
-		return s;
-	}
-	
-	private void initMurals(String path, String name) {
-		//murals.add(new Mural());
+	private void init(String path, String name) {
+		// initialize Memstore, possibly from a Logfile
+		// load possible SFiles
 	}
 
-	private void initSketchBook(String path, String name) throws IOException {
-		String filename = SketchesHelper.getFilename(path, name);
-		this.memory     = SketchesHelper.loadSketchBook(filename);
-		this.book       = new BufferedSketchBook(filename);
+	/*
+	 * ScheduledTask Robots start here.
+	 */
+	private class Flusher implements Runnable {
+
+		public void run() {
+
+			readLock.lock();
+
+			try {
+				
+				writer.flush();
+
+			} catch (IOException e) {
+				logger.error("Flusher: couldn't flush!", e);
+			} finally {
+				readLock.unlock();
+			}
+		}
+	}
+	
+	private class Scriber implements Runnable {
+		private final int threshold; // maxSize of Memstore in bytes
+		
+		public Scriber(int threshold) {
+			this.threshold = threshold * 1024 * 1024; 
+		}
+
+		public void run() {
+
+			readLock.lock();
+			
+			if (writer.getSize() > threshold) {
+				readLock.unlock();
+				writeLock.lock();
+			
+				// as we are the only one modifying this datastructure and
+				// we have just one bomber each time, we don't really need
+				// to check again for the condition.
+				assert writer.getSize() > threshold: "Scriber: memory has shrunk between two checks!";
+				assert readers.get(0) instanceof Memstore: "head of readers is not a Memstore";
+				
+				Memstore oldStore = (Memstore) readers.get(0);
+				RAMSFile ramSFile = new RAMSFile(oldStore);
+				Memstore newStore = new Memstore();
+
+				readers.set(0, newStore); // substitute old with new clean store
+				readers.add(1, ramSFile); // insert temporary SFile
+				writer = newStore;
+				
+				writeLock.unlock();
+				
+				// convert to SFile
+				SFile newSFile = null;
+				try {
+					newSFile = SketchesHelper.scribe(oldStore);
+				} catch (IOException e) {
+					logger.error("Scriber: failed scribing " + oldStore, e);
+				}
+				
+				writeLock.lock();
+				
+				// switch temporary RAMSFile with fresh new SFile
+				readers.set(readers.indexOf(ramSFile), newSFile);
+				
+				writeLock.unlock();
+				
+				scheduleCompaction();
+			}
+		}
+
+		private void scheduleCompaction() {
+			// run/schedule only if not already running
+			if (compactorHandle != null && compactorHandle.isDone())
+				compactorHandle = compactorScheduler.schedule(new Compactor(), 0, TimeUnit.SECONDS);
+		}
+	}
+	
+	private class Compactor implements Runnable {
+		
+		public void run() {
+			
+		}
 	}
 }
--- src/main/java/org/acaro/sketches/SketchesHelper.java
+++ src/main/java/org/acaro/sketches/SketchesHelper.java
@@ -23,22 +23,20 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.FileChannel.MapMode;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
-import org.acaro.sketches.mindsketches.MindSketches;
-import org.acaro.sketches.mural.FSMuralIterator;
-import org.acaro.sketches.mural.FSMuralWriter;
-import org.acaro.sketches.mural.FSMuralsCursor;
-import org.acaro.sketches.sketch.Sketch;
-import org.acaro.sketches.sketch.SketchHelper;
-import org.acaro.sketches.util.SketchComparator;
+import org.acaro.sketches.memstore.Memstore;
+import org.acaro.sketches.operation.Operation;
+import org.acaro.sketches.operation.OperationComparator;
+import org.acaro.sketches.operation.OperationHelper;
+import org.acaro.sketches.sfile.FSSFile;
+import org.acaro.sketches.sfile.FSSFileIterator;
+import org.acaro.sketches.sfile.FSSFileWriter;
+import org.acaro.sketches.sfile.FSSFileCursor;
+import org.acaro.sketches.sfile.SFile;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.primitives.UnsignedBytes;
 
 /**
  * 
@@ -52,30 +50,28 @@ public class SketchesHelper {
 	static final Logger logger = LoggerFactory.getLogger(SketchesHelper.class);
 	private static final String EXTENSION = ".sb";
 
-	public static MindSketches loadSketchBook(String file) throws IOException {
-		MindSketches memory = new MindSketches();
-		RandomAccessFile book = null;
-		FileChannel ch = null;
+	public static Memstore loadSketchBook(String file) throws IOException {
+		Memstore memory = new Memstore();
+		FileChannel ch  = null;
 		int loaded = 0;
 		
 		try {
-			book = new RandomAccessFile(file, "rw");
-			ch = book.getChannel();
+			ch = new RandomAccessFile(file, "rw").getChannel();
 			MappedByteBuffer buffer = ch.map(MapMode.READ_WRITE, 0, ch.size());
 			buffer.load();
 			
 			while (buffer.hasRemaining()) {
-				Sketch s = SketchHelper.readItem(buffer);				
+				Operation s = OperationHelper.readItem(buffer);				
 				memory.put(s.getKey(), s);
 				loaded++;
 			}
 			
-			book.close();
+			ch.close();
 		} catch (BufferUnderflowException e) {
 			logger.info("Truncated file, we probably died without synching correctly");
 			ch.truncate(ch.position());
 			ch.force(true);
-			book.close();
+			ch.close();
 		}
 
 		logger.debug(loaded + " loaded: " + memory.getSize());
@@ -88,36 +84,39 @@ public class SketchesHelper {
 	}
 	
 	
-	public static void bombMindSketches(MindSketches memory, String filename) throws IOException {
+	public static SFile scribe(Memstore memory) throws IOException {
 		long start = System.currentTimeMillis();
 		logger.info("burning started: " + start);
 
-		Map<byte[], Sketch> map = memory.getMap();
-		ArrayList<Sketch> sorted = new ArrayList<Sketch>(map.size());
+		Map<byte[], Operation> map = memory.getMap();
+		ArrayList<Operation> sorted = new ArrayList<Operation>(map.size());
 		sorted.addAll(map.values());
 
-		Collections.sort(sorted, new SketchComparator());
+		Collections.sort(sorted, new OperationComparator());
 		logger.debug("MindSketches is sorted: "+ (System.currentTimeMillis()-start));
 
-		FSMuralWriter writer = new FSMuralWriter(filename);
+		FSSFileWriter writer = new FSSFileWriter(memory.getFilename());
 		
 		start = System.currentTimeMillis();
-		for (Sketch sketch: sorted)
+		for (Operation sketch: sorted)
 			writer.write(sketch);
 		
 		writer.close();
 		logger.info("burning finished: " + (System.currentTimeMillis()-start));
+		
+		return new FSSFile("sticazzi");
 	}
 	
-	public static void scribeMurals(List<FSMuralIterator> muralIterators, String filename) throws IOException {
+	public static void compact(List<FSSFileIterator> muralIterators, String filename) throws IOException {
 		if (muralIterators.size() < 2) throw new IllegalArgumentException("Can't merge less than 2 Murals");
 		
-		FSMuralsCursor cursor = new FSMuralsCursor(muralIterators);
-		FSMuralWriter writer = new FSMuralWriter(filename);
+		FSSFileCursor cursor = new FSSFileCursor(muralIterators);
+		FSSFileWriter writer = new FSSFileWriter(filename);
 		
 		while (cursor.hasNext()) {
 			writer.write(cursor.next());
 		}
+		
 		cursor.close();
 		writer.close();
 	}
